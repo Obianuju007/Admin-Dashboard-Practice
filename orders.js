0c762b3ea61b543ccaa3e326fdcffd64b7803d66
Obianuju Onwuasoanya <onobes007@gmail.com>
2022-05-08 07:49:27 +0100

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,36 @@
+const Orders = [{
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        productStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        productStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Driver',
+        productNumber: '49347',
+        productStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '96996',
+        productStatus: 'Paid',
+        shipping: 'Delivered'
+    }, {
+        productName: 'DJI Air 2S',
+        productNumber: '81475',
+        productStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lozenge Drone',
+        productNumber: '00482',
+        productStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
